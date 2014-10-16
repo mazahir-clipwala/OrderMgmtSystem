ad68488588e6e2edaff6cc39e88fc537d174f3f1
max <clipwala.mazahir@gmail.com>
2014-10-16 17:03:39 -0500

    Updated

--- src/learnings/by/ali/Orders.java
+++ src/learnings/by/ali/Orders.java
@@ -1,11 +1,45 @@
 package learnings.by.ali;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
-@WebService
+@WebService (name="OrdersWS", serviceName = "MyOrders", portName = "MyOrdersPort", targetNamespace = "www.myorder.com")
 public class Orders {
+	List<String> allPOs = new ArrayList<String>();
 	
-	public String sendPO (String item, String customer) {
-		return "you were successful";
+	/*@WebResult (name = "yourPONumber")
+	public String sendPO (@WebParam (name = "products") String item, @WebParam (name = "custName")String customer) {
+		String poNumber = new BigInteger(130, new SecureRandom()).toString ();
+		
+		allPOs.add (poNumber);
+		
+		return poNumber;
+	}*/
+	
+	@WebMethod (action = "displayAllPOs", operationName = "displayAllPOs")
+	public List<String> showAllPOs () throws DBException {
+		int ii = 1;
+		if (ii == 0) {
+			throw new DBException("DB Down", "Your request could not be processed.");
+		}
+		return allPOs;
 	}
+	
+	/*public String sendPOv2 (PurchaseOrder order) {
+		String poNumber = new BigInteger(130, new SecureRandom()).toString ();
+		
+		allPOs.add (poNumber);
+		
+		return poNumber;
+		
+	}*/
+	
+	
 }
